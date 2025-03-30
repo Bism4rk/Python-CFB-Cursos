db8dff4fb3757a7e35d6d14a7f5a49240120eee7
Bismarck <brunowasborn2rock@gmail.com>
2025-03-30 16:12:19 -0300

    tkinter option menu

--- Aulas/9Tkinter/aula64.py
+++ Aulas/9Tkinter/aula64.py
@@ -7,4 +7,30 @@ app = Tk()
 app.title("CFB Cursos - Tkinter option menu")
 app.geometry("500x300")
 
+vesporte = StringVar()
+listaEsportes = ["Futebol", "Vôlei", "Basquete"]
+vesporte.set(listaEsportes[0]) # Valor padrão
+
+def imprimirEsporte():
+    ve = vesporte.get()
+    if ve == "Futebol":
+        print("Esporte: Futebol")
+    elif ve == "Vôlei":
+        print("Esporte: Vôlei")
+    elif ve == "Basquete":
+        print("Esporte: Basquete")
+    else: 
+        print("Selecione um esporte...")
+
+
+bl_esportes = Label(app, text="Esportes")
+bl_esportes.pack()
+
+op_esportes = OptionMenu(app, vesporte, *listaEsportes)
+op_esportes.pack()
+
+btn_esporte = Button(app, text="Selecionar esporte", command=imprimirEsporte)
+btn_esporte.pack()
+
+os.system('cls')
 app.mainloop()
